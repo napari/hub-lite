c4c5704e70c4f16e08395f7936f6c9b8f236df85
Draga Doncila <ddon0001@student.monash.edu>
2025-07-21 21:11:46 -0400

    Try redirecting

--- static/js/redirect_404.js
+++ static/js/redirect_404.js
@@ -1,8 +1,18 @@
-(function() {
+(function () {
     const path = window.location.pathname;
     const isPlugin = path.startsWith('/plugins/') && !path.endsWith('.html');
-
     if (isPlugin) {
-      window.location.replace(path + '.html');
+      // Extract plugin name (last part after last '/')
+      const parts = path.split('/');
+      const pluginName = parts[parts.length - 1];
+
+      const normalized = normalizeName(pluginName);
+      const newPath = path.slice(0, path.lastIndexOf('/') + 1) + normalized + '.html';
+      window.location.replace(newPath);
     }
-  })();
+
+    function normalizeName(name) {
+        return name.replace(/[-_.]+/g, "-").toLowerCase();
+  }
+
+})();
