4b8f4ff0ee4e052087e834dc2934c82c4858b81d
Draga Doncila <ddon0001@student.monash.edu>
2025-07-21 22:15:46 -0400

    Remove debugs and hub-lite root

--- static/js/redirect_404.js
+++ static/js/redirect_404.js
@@ -3,22 +3,18 @@
 (function() {
     const path = window.location.pathname;
     const isPlugin = path.includes('/plugins/');
-    console.log(isPlugin, path);
     if (isPlugin) {
       // Extract plugin name (last part after last '/')
       const parts = path.split('/');
       var pluginName = parts[parts.length - 1];
-      console.log(pluginName);
 
       if (pluginName.endsWith('.html')) {
         // Remove the '.html' extension, before reconstructing the URL
         pluginName = pluginName.slice(0, -5);
       }
-      console.log(pluginName);
 
       const normalized = normalizeName(pluginName);
       const newPath = path.slice(0, path.lastIndexOf('/') + 1) + normalized + '.html';
-      console.log(newPath)
       window.location.replace(newPath);
     }
 
