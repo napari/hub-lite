8cdefa2f12c87cc7962a3768ee5fb86589dabc81
Draga Doncila <ddon0001@student.monash.edu>
2025-07-21 21:41:39 -0400

    Add some debug prints

--- static/js/redirect_404.js
+++ static/js/redirect_404.js
@@ -3,6 +3,7 @@
 (function() {
     const path = window.location.pathname;
     const isPlugin = path.startsWith('/plugins/');
+    console.log(isPlugin, path);
     if (isPlugin) {
       // Extract plugin name (last part after last '/')
       const parts = path.split('/');
@@ -15,6 +16,7 @@
 
       const normalized = normalizeName(pluginName);
       const newPath = path.slice(0, path.lastIndexOf('/') + 1) + normalized + '.html';
+      console.log(newPath)
       window.location.replace(newPath);
     }
 
